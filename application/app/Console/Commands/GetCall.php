2dbbe6433461e9a8da070cbf1b6d6b6b785d950c
cy322666 <cy322666@gmail.com>
2023-04-13 17:08:57 +0200

    add scheduler

--- application/app/Console/Commands/GetCall.php
+++ application/app/Console/Commands/GetCall.php
@@ -6,8 +6,6 @@ use App\Models\Call;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Laravel\Octane\Exceptions\DdException;
-use Services\amoCRM\Client;
 use Symfony\Component\Console\Command\Command as CommandAlias;
 
 class GetCall extends Command

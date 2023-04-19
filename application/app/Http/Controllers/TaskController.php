ffc96afdf96ec86e93e44e4d89b78a96ea9353b9
cy322666 <cy322666@gmail.com>
2023-04-19 20:41:04 +0200

    add task

---
+++ application/app/Http/Controllers/TaskController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Services\amoCRM\Client;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class TaskController extends Controller
+{
+    /**
+     * @throws Exception
+     */
+    public function weekend(Request $request)
+    {
+        $dateTime = Carbon::parse($request->toArray()['task']['add'][0]['complete_till']);
+        $dayWeek  = Carbon::parse($request->toArray()['task']['add'][0]['complete_till'])->dayOfWeek;
+        $taskId   = $request->toArray()['task']['add'][0]['id'];
+
+        if ($dayWeek == 6 || $dayWeek == 7) {
+
+            Log::info(__METHOD__, ['day' => $dayWeek, 'task' => $taskId]);
+
+            $amoApi = (new Client(Account::query()->first()))->init();
+
+            $task = $amoApi->service->tasks()->find($taskId);
+
+            $completeTill = $dayWeek == 7 ? $dateTime->addDays(2) : $dateTime->addDay();
+
+            $task->complete_till = $completeTill->format('Y-m-d H:i:s');
+            $task->save();
+        }
+    }
+}
